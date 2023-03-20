528c1321210ed037025387d3c5ca27059263050b
Nam Nguyen Phuong <namnp081296@gmail.com>
2023-03-20 15:28:48 +0700

    Add the name of the webapp service

--- starter_files/make_predict_azure_app.sh
+++ starter_files/make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://<yourappname>.azurewebsites.net:$PORT/predict 
+     -X POST https://lemon-wave-41de4eec6948486d96c3809a828003b9.azurewebsites.net:$PORT/predict 
      # TODO: Replace the <yourappname> with your application name 
